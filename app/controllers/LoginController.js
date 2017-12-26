3f47441291d5919087171e5bb5ba01c9136043b5
Rajat <rajatvig71@gmail.com>
2017-12-26 11:39:39 +0530

    update Login apis path

--- app/controllers/LoginController.js
+++ app/controllers/LoginController.js
@@ -1,13 +1,13 @@
 module.exports = function (app, db) {
     // CRUD
     
-    app.get('/api/all', function (req, res) {
+    app.get('/api/login/all', function (req, res) {
         db.Login.findAll({}).then(function (result) {
             res.json(result);
         });
     });
     
-    app.post('/api/new', function (req, res) {
+    app.post('/api/login/new', function (req, res) {
         db.Login.create({
             username: req.body.username,
             passwordhash: req.body.passwordhash,
@@ -17,7 +17,7 @@ module.exports = function (app, db) {
         });
     });
     
-    app.put('/api/update/:id', function (req, res) {
+    app.put('/api/login/update/:id', function (req, res) {
         db.Login.update({
             username: req.body.username,
             passwordhash: req.body.passwordhash,
@@ -32,7 +32,7 @@ module.exports = function (app, db) {
         });
     });
     
-    app.delete('/api/delete/:id', function (req, res) {
+    app.delete('/api/login/delete/:id', function (req, res) {
         db.Login.destroy({
             where: {
                 userid: req.params.id
